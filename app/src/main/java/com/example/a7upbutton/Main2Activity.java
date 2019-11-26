5c707ccca5934410ddd5833039a725173be752ca
thanmanhvinh <thanmanhvinh@gmail.com>
2019-11-26 13:57:37 +0700

    display

---
+++ app/src/main/java/com/example/a7upbutton/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.example.a7upbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button btnOpenA3 = findViewById(R.id.btnOpenA3);
+
+        getSupportActionBar().setTitle("Activity 2");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnOpenA3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/a7upbutton/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a7upbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOpenA2 = findViewById(R.id.btnOpenA2);
+
+        getSupportActionBar().setTitle("Activity");
+
+        btnOpenA2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
